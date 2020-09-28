741efd637f26c677e209ca256f765a563804e59f
William Pereira <pereirawe@gmail.com>
2020-09-28 18:14:28 -0300

    Laravel Init

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // require_once('./Route.php');
-    // require_once './App/Views/index.php';
-    require_once './App/Routes.php';
-    $request = new Route;
-    $request->listen();
-
-?>
