2d00c57eb811bd00809ca5b0c7cf4ea02c247099
William Pereira <pereirawe@gmail.com>
2020-09-28 18:35:25 -0300

    Public htyml

--- public_html/Deploy/index.php
+++ public_html/Deploy/index.php
@@ -31,7 +31,7 @@
                     <pre  class="text-white"><ul><li ng-repeat="(key, value) in content"  ><b ng-bind="key | capitalize"></b>: <span ng-bind="value"></span></li></ul></pre>
                 </div>
 
-                
+
             </div>
         </div>
     </div>
@@ -40,7 +40,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/8ae5e4e13d.js" crossorigin="anonymous"></script>
-    <script src="../node_modules/angular/angular.min.js"></script>
-    <script src="app.js"></script>
+    <script src="../../node_modules/angular/angular.min.js"></script>
+    <script src="/app.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/test', function () {
     return 'test';
 });
